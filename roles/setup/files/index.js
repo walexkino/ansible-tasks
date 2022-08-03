6bf6c478aab2fb8a88d76aa56e64d7ee56a54a07
walexkino papy <waladino@yahoo.com>
2022-08-03 20:22:20 +0100

    changes to the node js file

--- roles/setup/files/index.js
+++ roles/setup/files/index.js
@@ -1,6 +1,6 @@
 var http = require("http");
 var server = http.createServer(function (req, res) {
 res.writeHead(200);
-res.end("Welcome to walexkino ansible home page \t The ansible playbook script worked!!");
+res.end("Welcome to walexkino ansible home page, The ansible playbook script worked!!");
 });
 server.listen(3000); // this port as already been allowed in the ingress port for the target ec2 instance
